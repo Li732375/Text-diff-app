b9c64e606c258ab6114f3000622d1ab2eeaf6087
Li732375 <lee732375@gmail.com>
2025-03-11 15:17:03 +0800

    主程式 1 統一程式變數。 2 獨立提示成變數，微調 Tab2 提示內容。 3 註解 Tab3。

--- src/main.py
+++ src/main.py
@@ -16,78 +16,81 @@ class TextDiffApp(QWidget):
         self.initUI_Tab1()
         self.initUI_Tab2()
         self.tabWidget.setTabEnabled(1, False)  # 預設隱藏 Diff Tab
-        self.initUI_Tab3()
-        self.tabWidget.setTabEnabled(2, False)  # 預設隱藏 Diff Tab
+        """ self.initUI_Tab3()
+        self.tabWidget.setTabEnabled(2, False)  # 預設隱藏 Diff Tab """
 
         mainlayout.addWidget(self.tabWidget)
         self.setLayout(mainlayout)
 
     def initUI_Tab1(self):
         # Input Tab
-        self.textEdit1 = QTextEdit()
-        self.textEdit1.setPlaceholderText("Enter first text here...")
-        self.textEdit1.setStyleSheet("font-size: 16px;")
+        self.textEdit_Up = QTextEdit()
+        self.textEdit_Up.setPlaceholderText("Enter first text here...")
+        self.textEdit_Up.setStyleSheet("font-size: 16px;")
 
-        self.textEdit2 = QTextEdit()
-        self.textEdit2.setPlaceholderText("Enter second text here...")
-        self.textEdit2.setStyleSheet("font-size: 16px;")
+        self.textEdit_Down = QTextEdit()
+        self.textEdit_Down.setPlaceholderText("Enter second text here...")
+        self.textEdit_Down.setStyleSheet("font-size: 16px;")
 
         self.compareButton = QPushButton('Compare')
         self.compareButton.clicked.connect(self.compareTexts)
 
-        self.inputTab = QWidget()
+        self.widget_InputTab = QWidget()
 
-        inputTabLayout = QVBoxLayout(self.inputTab)
-        inputTabLayout.addWidget(self.textEdit1)
-        inputTabLayout.addWidget(self.textEdit2)
-        inputTabLayout.addWidget(self.compareButton)
+        Vlayout_InputTab = QVBoxLayout(self.widget_InputTab)
+        Vlayout_InputTab.addWidget(self.textEdit_Up)
+        Vlayout_InputTab.addWidget(self.textEdit_Down)
+        Vlayout_InputTab.addWidget(self.compareButton)
 
-        self.tabWidget.addTab(self.inputTab, "Input")
-        self.tabWidget.setTabToolTip(self.tabWidget.indexOf(self.inputTab), "輸入要比較的文字")
+        self.tabWidget.addTab(self.widget_InputTab, "Input")
+        self.tabWidget.setTabToolTip(self.tabWidget.indexOf(self.widget_InputTab), "輸入要比較的文字")
 
     def initUI_Tab2(self):
         # Diff Line Tab
-        self.leftTextEdit = QPlainTextEdit()
-        self.leftTextEdit.setReadOnly(True)
-        self.leftTextEdit.setLineWrapMode(QPlainTextEdit.NoWrap)
-        self.leftTextEdit.setFont(QFont("Courier New", 10))
-        self.leftTextEdit.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.leftTextEdit.verticalScrollBar().valueChanged.connect(self.syncScroll)
+        self.pTextEdit_Left = QPlainTextEdit()
+        self.pTextEdit_Left.setReadOnly(True)
+        self.pTextEdit_Left.setLineWrapMode(QPlainTextEdit.NoWrap)
+        self.pTextEdit_Left.setFont(QFont("Courier New", 10))
+        self.pTextEdit_Left.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.pTextEdit_Left.verticalScrollBar().valueChanged.connect(self.syncScroll)
 
-        self.rightTextEdit = QPlainTextEdit()
-        self.rightTextEdit.setReadOnly(True)
-        self.rightTextEdit.setLineWrapMode(QPlainTextEdit.NoWrap)
-        self.rightTextEdit.setFont(QFont("Courier New", 10))
-        self.rightTextEdit.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.rightTextEdit.verticalScrollBar().valueChanged.connect(self.syncScroll)
+        self.pTextEdit_Right = QPlainTextEdit()
+        self.pTextEdit_Right.setReadOnly(True)
+        self.pTextEdit_Right.setLineWrapMode(QPlainTextEdit.NoWrap)
+        self.pTextEdit_Right.setFont(QFont("Courier New", 10))
+        self.pTextEdit_Right.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.pTextEdit_Right.verticalScrollBar().valueChanged.connect(self.syncScroll)
 
         # 建立水平佈局
-        self.diffHLayout = QHBoxLayout()
-        self.diffHLayout.addWidget(self.leftTextEdit)
-        self.diffHLayout.addWidget(self.rightTextEdit)
+        self.Hlayout_DiffLineTab = QHBoxLayout()
+        self.Hlayout_DiffLineTab.addWidget(self.pTextEdit_Left)
+        self.Hlayout_DiffLineTab.addWidget(self.pTextEdit_Right)
 
         # 控制水平佈局伸展方向，限制兩個 QTextEdit 視窗寬度延伸
-        self.diffWidget = QWidget()
-        self.diffWidget.setLayout(self.diffHLayout)  # 設定水平佈局
+        self.widget_DiffLineTab = QWidget()
+        self.widget_DiffLineTab.setLayout(self.Hlayout_DiffLineTab)  # 設定水平佈局
         
         # 加入 tab 頁
-        self.tabWidget.addTab(self.diffWidget, "Diff Line Result")
-        self.tabWidget.setTabToolTip(self.tabWidget.indexOf(self.diffWidget), "行比對結果，" + 
-        "\n滾輪：垂直卷軸 " +
-        "\nShfit + 滾輪：兩邊垂直卷軸" +
-        "\nCtrl + 滾輪：內容縮放" +
-        "\nAlt + 滾輪：水平卷軸 ")
+        self.tabWidget.addTab(self.widget_DiffLineTab, "Diff Line Result")
 
-    def initUI_Tab3(self):
+        tabTip = "行比對結果" +\
+        "\n滾輪：垂直卷軸 " +\
+        "\nShfit + 滾輪：兩邊垂直卷軸" +\
+        "\nCtrl + 滾輪：內容縮放" +\
+        "\nAlt + 滾輪：水平卷軸 "
+
+        self.tabWidget.setTabToolTip(self.tabWidget.indexOf(self.widget_DiffLineTab), tabTip)
+
+    """ def initUI_Tab3(self):
         # Diff Word Tab
         
         
         # 加入 tab 頁
-        self.tabWidget.addTab(self.diffWidget, "Diff Word Result")
-        self.tabWidget.setTabToolTip(self.tabWidget.indexOf(self.diffWidget), "詞比對結果")
+        self.tabWidget.addTab(, "Diff Word Result")
+        self.tabWidget.setTabToolTip(self.tabWidget.indexOf(self.widget_DiffLineTab), "詞比對結果") """
 
     def compareTexts(self):
-        text1, text2 = self.textEdit1.toPlainText(), self.textEdit2.toPlainText()
+        text1, text2 = self.textEdit_Up.toPlainText(), self.textEdit_Down.toPlainText()
         differ = Differ()
         diff = list(differ.compare(text1.splitlines(), text2.splitlines()))
 
@@ -104,11 +107,11 @@ class TextDiffApp(QWidget):
                 leftText.append(line[2:])
                 rightText.append(line[2:])
 
-        self.leftTextEdit.setPlainText('\n'.join(leftText))
-        self.rightTextEdit.setPlainText('\n'.join(rightText))
+        self.pTextEdit_Left.setPlainText('\n'.join(leftText))
+        self.pTextEdit_Right.setPlainText('\n'.join(rightText))
 
         # 差異行色彩強調
-        cursor1, cursor2 = self.leftTextEdit.textCursor(), self.rightTextEdit.textCursor()
+        cursor1, cursor2 = self.pTextEdit_Left.textCursor(), self.pTextEdit_Right.textCursor()
         cursor1.movePosition(QTextCursor.Start)
         cursor2.movePosition(QTextCursor.Start)
 
@@ -129,15 +132,15 @@ class TextDiffApp(QWidget):
             cursor2.movePosition(QTextCursor.NextBlock)
 
         self.tabWidget.setTabEnabled(1, True)
-        self.tabWidget.setCurrentWidget(self.diffWidget)
+        self.tabWidget.setCurrentWidget(self.widget_DiffLineTab)
 
     def syncScroll(self, value):
         if QApplication.keyboardModifiers() == Qt.ShiftModifier:
             sender = self.sender()
-            if sender == self.leftTextEdit.verticalScrollBar():
-                self.rightTextEdit.verticalScrollBar().setValue(value)
-            elif sender == self.rightTextEdit.verticalScrollBar():
-                self.leftTextEdit.verticalScrollBar().setValue(value)
+            if sender == self.pTextEdit_Left.verticalScrollBar():
+                self.pTextEdit_Right.verticalScrollBar().setValue(value)
+            elif sender == self.pTextEdit_Right.verticalScrollBar():
+                self.pTextEdit_Left.verticalScrollBar().setValue(value)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
